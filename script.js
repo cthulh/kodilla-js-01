32ab8a441f81e9f241d8c84bbaa4f44583272386
Mariusz Borowiak <borowiakm85@gmail.com>
2018-04-26 08:08:42 +0100

    Add writeTo and appendElement func

--- script.js
+++ script.js
@@ -17,15 +17,25 @@ function decimalFormat(num,dec){
   return parseFloat(num).toFixed(dec)
 }
 
+// Write to an element
+function writeTo(element, content){
+  element.innerHTML = content;
+}
+
+// Append element
+function appendElement(element, content){
+  element.innerHTML += content;
+}
+
 // Water form
 function updateWaterForm(temp){
   console.log("Water form in : " + temp);
   if (temp <= 0) {
-    waterForm.innerHTML = "WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is frozen solid!";
+    writeTo(waterForm,"WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is frozen solid!");
   } else if (temp > 0 && temp <= 99) {
-    waterForm.innerHTML = "WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is liquid!";
+    writeTo(waterForm,"WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is liquid!");
   } else {
-    waterForm.innerHTML = "WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is in gas form!";
+    writeTo(waterForm,"WATER FORM: Water in " + decimalFormat(celcTemp,2) + " C is in gas form!");
   }
 }
 
@@ -33,65 +43,65 @@ function updateWaterForm(temp){
 function updateWeatherClothing(temp){
   console.log("Weather clothing in : " + temp);
   if (temp > -40 && temp <= 0) {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need warm winter jacket and gloves!";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need warm winter jacket and gloves!");
   } else if (temp > 0 && temp <= 10) {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need a hat and extra layers!";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need a hat and extra layers!");
   } else if (temp > 10 && temp <= 18) {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you can wear a t shirt and trousers";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you can wear a t shirt and trousers");
   } else if (temp > 18 && temp <= 30) {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you can wear a t shirt and shorts";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you can wear a t shirt and shorts");
   } else if (temp >= 30 && temp <= 41) {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, it's time to hide in shade!";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, it's time to hide in shade!");
   } else {
-    weatherClothing.innerHTML = "DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need to stay indoors or you will die...";
+    writeTo(weatherClothing,"DRESS FOR THE WEATHER: If it's " + decimalFormat(celcTemp,2) + " C, you need to stay indoors or you will die...");
   }
 }
 
 // Temp trivia
 function updateTempTrivia(temp){
   console.log("Temp trivia in : " + temp);
-  tempTrivia.innerHTML = "<br>Temperature trivia:<br><ul>";
+  writeTo(tempTrivia,"<br>Temperature trivia:<br><ul>");
   if (temp < -31.00) {
-    tempTrivia.innerHTML += "<li>Temperature is lower than -31C, the lowest operating temperaturte of a most resistant mobile phone!"
+    appendElement(empTrivia,"<li>Temperature is lower than -31C, the lowest operating temperaturte of a most resistant mobile phone!");
   }
   if (temp < -39.00) {
-    tempTrivia.innerHTML += "<li>Temperature is lower than -39C, the melting temperature of mercury!"
+    appendElement(tempTrivia,"<li>Temperature is lower than -39C, the melting temperature of mercury!");
   }
   if (temp < -216.00) {
-    tempTrivia.innerHTML += "<li>Temperature is lower than -216C, the coldest planet in the Solar System, Uranus!"
+    appendElement(tempTrivia,"<li>Temperature is lower than -216C, the coldest planet in the Solar System, Uranus!");
   }
   if (temp === 0.00) {
-    tempTrivia.innerHTML += "<li>Temperature is 0C, the melting temperature of ice"
+    appendElement(tempTrivia,"<li>Temperature is 0C, the melting temperature of ice");
   }
   if (temp < 13.70) {
-    tempTrivia.innerHTML += "<li>Temperature below 13.7C, the lowest body temperature recorded in a human!"
+    appendElement(tempTrivia,"<li>Temperature below 13.7C, the lowest body temperature recorded in a human!");
   }
   if (temp < 37.00) {
-    tempTrivia.innerHTML += "<li>Temperature below 37C, the normal body temperature in a human!"
+    appendElement(tempTrivia,"<li>Temperature below 37C, the normal body temperature in a human!");
   }
   if (temp === 37.00) {
-    tempTrivia.innerHTML += "<li>Temperature is 37C, the normal body temperature in a human!"
+    appendElement(tempTrivia,"<li>Temperature is 37C, the normal body temperature in a human!");
   }
   if (temp > 37.00) {
-    tempTrivia.innerHTML += "<li>Temperature over 37C, the normal body temperature in a human!"
+    appendElement(tempTrivia,"<li>Temperature over 37C, the normal body temperature in a human!");
   }
   if (temp > 46.50) {
-    tempTrivia.innerHTML += "<li>Temperature over 46.5C, the highest body temperature in a human!"
+    appendElement(tempTrivia,"<li>Temperature over 46.5C, the highest body temperature in a human!");
   }
   if (temp > 71.00) {
-    tempTrivia.innerHTML += "<li>Temperature over 71C, the highest surface temperature on Earth, recorded in Iran in 2005!"
+    appendElement(tempTrivia,"<li>Temperature over 71C, the highest surface temperature on Earth, recorded in Iran in 2005!");
   }
   if (temp > 101.00) {
-    tempTrivia.innerHTML += "<li>Temperature over 101C, the average surface temperature on the Moon during the day!"
+    appendElement(tempTrivia,"<li>Temperature over 101C, the average surface temperature on the Moon during the day!");
   }
 
-  tempTrivia.innerHTML +="</ul>"
+  appendElement(tempTrivia,"</ul>");
 }
 
 // Update temperatures
 function updateTemperatures(celcTemp, farnhTemp){
-  celcOutput.innerHTML = "Temperature in C: " + decimalFormat(celcTemp,2);
-  farnhOutput.innerHTML = "Temperature in F: " + decimalFormat(farnhTemp,2);
+  writeTo(celcOutput,"Temperature in C: " + decimalFormat(celcTemp,2));
+  writeTo(farnhOutput,"Temperature in F: " + decimalFormat(farnhTemp,2));
 }
 
 // Updater
